d8dd29b978ff3c4780cb948cd8aa2d2aa07e7323
mekrograca <grkem.squash99@gmail.com>
2023-01-03 18:49:14 +0300

    first

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,10 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+<script setup>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
 </script>
+<template>
+    <div>
+    </div>
+</template>
+<style scoped>
+
+</style>
---
+++ src/views/template/EmailView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import IconComponent from '@/components/IconComponent.vue';
+</script>
+<template>
+    <div id="EmailView" class="bg-g-ray-50 h-screen">
+        <div class="flex items-center p-2">
+            <div class="flex justify-start items-center px-3.5">
+                <IconComponent iconString="menu" iconColor="#636363" :iconSize=19 hoverColor="hover:bg-gray-200" text="Main Menu" class="-ml2 mr-2" />
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+
+</style>
